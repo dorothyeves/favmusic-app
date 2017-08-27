17668ed48905685cebcbd82aa765e17f680bc0a6
Dorothy Eves <dorothyeves7@gmail.com>
2017-08-27 02:01:03 +0000

    added submit button and parameters to check for input

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,4 +6,15 @@ class ArtistsController < ApplicationController
   def new
     @artist = Artist.new
   end
+
+  def create
+    Artist.create(artist_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :description)
+  end
 end
